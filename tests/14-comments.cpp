d4d78727133d64cc9a927dd5d3ecf48030e4bfe9
Alexey Fedoseev <aleksey@fedoseev.net>
2024-04-25 10:06:47 +0300

    comment test

---
+++ tests/14-comments.cpp
@@ -0,0 +1,55 @@
+/* -----------------------------------------------------------------------------
+ * The Cyberiada GraphML C++ library implemention
+ *
+ * The test
+ *
+ * Copyright (C) 2024 Alexey Fedoseev <aleksey@fedoseev.net>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see https://www.gnu.org/licenses/
+ * ----------------------------------------------------------------------------- */
+
+#include <iostream>
+#include "cyberiadamlpp.h"
+#include "testutils.h"
+
+using namespace Cyberiada;
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	Document d;
+
+	StateMachine* sm = d.new_state_machine("SM");
+	Comment* comm = d.new_comment(sm, "Top level");
+	try {
+		// check id uniqueness
+		d.new_comment(sm, "n0", "testname", "Testbody");
+	} catch (const Cyberiada::ParametersException&){
+	}
+	State* state = d.new_state(sm, "State", Action(actionEntry, "action();"));
+	Comment* comm2 = d.new_comment(state, "Comment inside a state\nwith two lines");
+	d.new_formal_comment(sm, "Name", "Named formal comment");
+
+	d.add_comment_to_element(comm, state);
+	d.add_comment_to_element_name(comm2, state, "S");
+	d.add_comment_to_element_body(comm2, state, "action");
+
+	try {
+		cout << d << endl;
+		d.save(string(argv[0]) + ".graphml", formatCyberiada10);
+	} catch (const Cyberiada::Exception&) {
+		return 1;
+	}
+	return 0;
+}
